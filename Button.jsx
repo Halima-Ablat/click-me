7b74a4552f67dd10b9df98bd3731b641adae553a
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2024-10-23 20:15:22 +1100

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,17 @@
+function Button() {
+  let count = 0;
+  const clickButton = (e) => {
+    if (count <= 3) {
+      count++;
+      e.target.textContent = "Ouch 😒";
+    } else if (count <= 6) {
+      count++;
+      e.target.textContent = "STOP CLIKING ME 😩";
+    } else {
+      count++;
+      e.target.style.display = "none";
+    }
+  };
+  return <button onClick={(e) => clickButton(e)}>Click me 😎</button>;
+}
+export default Button;
